615105e86d23622dc191a006e6ccc73fa48493da
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-09-30 15:44:13 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 using namespace std;
+int main() 
+{
+  int n1, n2, n3, massimo;
+  cout << "qual è il primo numero?" << endl;
+  cin >> n1;
+  cout << "qual è il secondo numero?" << endl;
+  cin >> n2;
+  cout << "qual è il terzo numero?" << endl;
+  cin >> n3;
+  if (n1>n2)
+  {
+    massimo=n1;
+  }
+  else
+  {
+    if (n2>n3)
+    {
+      massimo=n2;
+    }
+    else
+    {
+      massimo = n3;
+    }
+  }
+   cout << "il numero maggiore è " << massimo << endl;
+  }
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
